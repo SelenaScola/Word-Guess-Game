2a1206635b6b1eb3ca3243840553d3868784ef03
SelenaScola <ss@byimagination.com>
2019-05-23 23:36:27 -0700

    added mor thought proccess, incomplete code.

---
+++ assets/javascript/game.js
@@ -0,0 +1,73 @@
+var wins = 0;
+var losses = 0;
+var remainingGuess = 9;
+
+// Determines which key was presse
+var userGuess = event.key; 
+
+//  lists all letters of the alphabet aka all lowercasr letter options available for user imputs.
+var letterOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+    "t", "u", "v", "w", "x", "y", "z"]
+
+// This is a function. It runs whenever the user presses a key and needs to update wins, guesses left, and losses.
+document.onkeyup = function (event) {
+userGuess === GuessesSoFar++ 
+uessesSoFar++ = wins++ || losses++ 
+// print win or loose, and always uessesSoFar
+
+
+for (var i = 0; i = userGuess.length; i++) {
+    // for loop (still figuring our how to correctly formulate the opperation)
+}
+// is the output of the computer that will need a for loop.
+var computerGuess = AssignRandomLetter; 
+// assign random leter from letter options to the computer guess
+var assignRandomLetter = indexOf.letterOptions;
+
+// someThing.someWhere[i];
+
+// compare userGuess to computer guess === win yay!
+if (userGuess === computerGuess) {
+    var winsText = document.getElementById("wins-text");
+    // incermint wins
+   var wins = wins++
+    // add wins to "P" htmlm tag
+    // reset remaining guesses to 9
+    remainingGuess + 9;
+    // reassign new computer guess
+}
+
+else (
+        var losses++ lossesText = document.getElementById("losses-text");
+)
+
+        text(letters[i]);
+
+
+}
+if (options.indexOf(userGuess) > -1) {
+
+
+    function updateScore() {
+        document.querySelector("#score").innerHTML = "Score: " + score;
+    }
+    <p id="userWins">Wins:</p>
+
+    // (# of times the user has guessed the letter correctly)
+
+    var game = {
+
+< p id = "userLosses" > Losses:</p>
+        // (# of times the user has failed to guess the letter correctly after exhausting all guesses)
+
+        <p id="guessesLeft">Guesses Left:</p>
+        // numGuesses
+
+        // (# of guesses left. This will update)
+
+        <p id="yourGuessesSoFar">Your Guesses So Far:</p>
+    // (the specific letters that the user typed. Display these until the user either wins or loses.)
+    };
+
+  
+    </script >
